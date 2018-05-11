5504d3d84d99e37fd3a0f1afa7037a39584bcabb
raphaeldobu <jose.bu@ccc.ufcg.edu.br>
2018-05-11 20:04:05 -0300

    model, repository e controller de cliente

---
+++ src/main/java/com/projetoposto/ws/model/Cliente.java
@@ -0,0 +1,60 @@
+package com.projetoposto.ws.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente {
+	
+	@Id @GeneratedValue
+	private Long id;
+	@Column
+	private String nome;
+	@Column
+	private String username;
+	@Column
+	private String password;
+	
+	public Cliente(){
+		
+	}
+	
+
+	public Cliente(Long id, String nome, String username, String password) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.username = username;
+		this.password = password;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
---
+++ src/main/java/com/projetoposto/ws/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.projetoposto.ws.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.projetoposto.ws.model.Cliente;
+
+public interface ClienteRepository extends CrudRepository<Cliente, Long>{
+	
+	
+
+}
