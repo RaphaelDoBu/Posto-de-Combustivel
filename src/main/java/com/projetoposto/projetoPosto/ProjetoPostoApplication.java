e5ecd4275fe8b1bdf6bac7c15d0de769f49a7802
raphaeldobu <jose.bu@ccc.ufcg.edu.br>
2018-05-11 18:44:53 -0300

    initial

---
+++ src/main/java/com/projetoposto/projetoPosto/ProjetoPostoApplication.java
@@ -0,0 +1,12 @@
+package com.projetoposto.projetoPosto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPostoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPostoApplication.class, args);
+	}
+}
