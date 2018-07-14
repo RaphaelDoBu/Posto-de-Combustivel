59b98c312290d0540b2346fb72ac6420e15aa13e
raphaeldobu <jose.bu@ccc.ufcg.edu.br>
2018-07-13 23:39:13 -0300

    documentação de cliente

--- src/main/java/com/projetoposto/cliente/ClienteController.java
+++ src/main/java/com/projetoposto/cliente/ClienteController.java
@@ -28,26 +28,31 @@ public class ClienteController {
 	private UserService userSerice;
 																																																																																																													
     @RequestMapping(method=RequestMethod.GET, value="/clientes")
+    @ApiOperation(value = "Listagem de todos os clientes")
 	public List<Cliente> getClientes(){
 		return clienteService.findAll();
 	}
 	
     @RequestMapping(method=RequestMethod.GET, value="/cliente/{id}")
-	public Optional<Cliente> getPosto(@PathVariable("id") Long id){
+    @ApiOperation(value = "Listagem de um cliente")
+	public Optional<Cliente> getCliente(@PathVariable("id") Long id){
 		return clienteService.findById(id);
 	}
 	
     @RequestMapping(method=RequestMethod.POST, value="/cliente")
-	public Cliente cadastroPosto(@RequestBody Cliente cliente){
+    @ApiOperation(value = "Criação de um clientes")
+	public Cliente cadastroCliente(@RequestBody Cliente cliente){
 		return userSerice.save(cliente);
 	}
 	
     @RequestMapping(method=RequestMethod.DELETE, value="/cliente/{id}")
-    public void	deletarPosto(Cliente cliente){
+    @ApiOperation(value = "Deletar um clientes")
+    public void	deletarCliente(Cliente cliente){
 		clienteService.delete(cliente);
 	}
 	
     @RequestMapping(method=RequestMethod.PUT, value="/cliente/{id}")
+    @ApiOperation(value = "Atualização dos dados de um clientes")
 	public Cliente updateCliente(Cliente cliente){
 		return userSerice.save(cliente);
 	}
